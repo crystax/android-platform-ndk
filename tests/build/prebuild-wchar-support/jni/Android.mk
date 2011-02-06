d29a54501c3e99a01cd9d105de689cffb4a11097
Dmitry Moskalchuk <dm@crystax.net>
2011-02-07 02:53:20 +0300

    Add test for wchar-support

--- tests/build/prebuild-wchar-support/jni/Android.mk
+++ tests/build/prebuild-wchar-support/jni/Android.mk
@@ -1,16 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
-$(call import-module,wchar-support)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE           := build_wchar_static
-LOCAL_SRC_FILES        := test-wchar.c test-c.c test-cpp.cpp
+LOCAL_SRC_FILES        := build-wchar.c
 LOCAL_STATIC_LIBRARIES := wchar_static
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE           := build_wchar_shared
-LOCAL_SRC_FILES        := test-wchar.c test-c.c test-cpp.cpp
+LOCAL_SRC_FILES        := build-wchar.c
 LOCAL_SHARED_LIBRARIES := wchar_shared
 include $(BUILD_EXECUTABLE)
 
