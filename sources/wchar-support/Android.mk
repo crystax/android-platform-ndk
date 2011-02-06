c511c76f4a311e471781616c41625b8171b1d347
Dmitry Moskalchuk <dm@crystax.net>
2011-02-06 23:34:01 +0300

    Modify path to sources and includes in wchar-support

--- sources/wchar-support/Android.mk
+++ sources/wchar-support/Android.mk
@@ -1,15 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-WCHAR_SRC_FILES :=
+WCHAR_SRC_FILES := wcstombs.c
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := wchar_static
-LOCAL_SRC_FILES         := $(WCHAR_SRC_FILES)
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_SRC_FILES         := $(addprefix src/,$(WCHAR_SRC_FILES))
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := wchar_shared
-LOCAL_SRC_FILES         := $(WCHAR_SRC_FILES)
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_SRC_FILES         := $(addprefix src/,$(WCHAR_SRC_FILES))
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(BUILD_SHARED_LIBRARY)
