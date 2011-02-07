fd1862a86b670bc8fe28bf9e6a0eef99dc123b70
Dmitry Moskalchuk <dm@crystax.net>
2011-02-07 03:25:18 +0300

    Add actual wchar tests (import them from FreeBSD)

--- tests/device/test-wchar/jni/Android.mk
+++ tests/device/test-wchar/jni/Android.mk
@@ -1,6 +1,26 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := test_wchar
-LOCAL_SRC_FILES := main.c
+
+LOCAL_SRC_FILES := main.c \
+	test-btowc.c \
+	test-iswctype.c \
+	test-mblen.c \
+	test-mbrlen.c \
+	test-mbrtowc.c \
+	test-mbsnrtowcs.c \
+	test-mbsrtowcs.c \
+	test-mbstowcs.c \
+	test-mbtowc.c \
+	test-towctrans.c \
+	test-wcrtomb.c \
+	test-wcsnrtombs.c \
+	test-wcsrtombs.c \
+	test-wcstombs.c \
+	test-wctomb.c \
+
+LOCAL_STATIC_LIBRARIES := wchar_static
+
 include $(BUILD_EXECUTABLE)
--- tests/device/test-wchar/jni/main.c
+++ tests/device/test-wchar/jni/main.c
@@ -1,4 +1,24 @@
+#ifdef DO_WCHAR_TEST
+#undef DO_WCHAR_TEST
+#endif
+#define DO_WCHAR_TEST(name) if (test_ ## name () != 0) return 1
+
 int main()
 {
+    DO_WCHAR_TEST(btowc);
+    DO_WCHAR_TEST(iswctype);
+    DO_WCHAR_TEST(mblen);
+    DO_WCHAR_TEST(mbrlen);
+    DO_WCHAR_TEST(mbrtowc);
+    DO_WCHAR_TEST(mbsnrtowcs);
+    DO_WCHAR_TEST(mbsrtowcs);
+    DO_WCHAR_TEST(mbstowcs);
+    DO_WCHAR_TEST(mbtowc);
+    DO_WCHAR_TEST(towctrans);
+    DO_WCHAR_TEST(wcrtomb);
+    DO_WCHAR_TEST(wcsnrtombs);
+    DO_WCHAR_TEST(wcsrtombs);
+    DO_WCHAR_TEST(wcstombs);
+    DO_WCHAR_TEST(wctomb);
     return 0;
 }
