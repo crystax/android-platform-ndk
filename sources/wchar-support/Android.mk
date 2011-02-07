cbaea124e7036f1cb84557e03017c244561df950
Dmitry Moskalchuk <dm@crystax.net>
2011-02-07 03:45:45 +0300

    More implementations added to wchar-support library

--- sources/wchar-support/Android.mk
+++ sources/wchar-support/Android.mk
@@ -11,17 +11,28 @@ WCHAR_SRC_FILES := \
 	locale/rune.c \
 	locale/runeglue.c \
 	locale/runetable.c \
+	locale/wcstod.c \
+	locale/wcstof.c \
+	locale/wcstoimax.c \
+	locale/wcstol.c \
+	locale/wcstold.c \
+	locale/wcstoll.c \
+	locale/wcstoul.c \
+	locale/wcstoull.c \
+	locale/wcstoumax.c \
+	locale/wctob.c \
+	locale/wctomb.c \
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := wchar_static
 LOCAL_SRC_FILES         := $(addprefix src/,$(WCHAR_SRC_FILES))
-LOCAL_C_INCLUDES        := $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES        := $(LOCAL_PATH)/include $(LOCAL_PATH)/src/locale
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := wchar_shared
 LOCAL_SRC_FILES         := $(addprefix src/,$(WCHAR_SRC_FILES))
-LOCAL_C_INCLUDES        := $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES        := $(LOCAL_PATH)/include $(LOCAL_PATH)/src/locale
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(BUILD_SHARED_LIBRARY)
