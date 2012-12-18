02cc8f156797562b65631bc4c90858c4f7c63425
Alexander Zhukov <zuav@crystax.net>
2012-12-18 20:14:48 +0400

    Commented debug output.
    Disable tests for %a and %A if GCC version is 4.4.3. There are some
    problems with long double in GCC 4.4.3 for Android it seems.

--- tests/device/test-printf/jni/main.c
+++ tests/device/test-printf/jni/main.c
@@ -2,6 +2,14 @@
 
 int main()
 {
+    /* int sz1 = sizeof(123.4567890123); */
+    /* int sz2 = sizeof(123.4567890123L); */
+    /* int sz3 = sizeof(long double); */
+
+    /* printf("!!! sizeof(123.4567890123)   = %d\n",  sz1); */
+    /* printf("!!! sizeof(123.4567890123L)  = %d\n",  sz2); */
+    /* printf("!!! sizeof(long double)      = %d\n",  sz3); */
+
     DO_PRINTF_TEST(printf);
     DO_PRINTF_TEST(fprintf);
     DO_PRINTF_TEST(sprintf);
