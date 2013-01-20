0e5d9c5e1e357af0d27b009e34f6d5733a59dc6e
Alexander Zhukov <zuav@crystax.net>
2013-01-20 21:25:48 +0400

    Add _Exit and strtold into crystax lib.

---
+++ tests/device/test-c99-stdlib/jni/main.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    assert(sizeof(double) == sizeof(long double));
+
+    double d = strtod("12.345678", NULL);
+    long double ld = strtold("12.345678", NULL);
+
+    /* printf("d = %f\n", d); */
+    /* printf("ld = %lf\n", ld); */
+
+    if (d != ld) {
+        printf("error: strtold produces unexptected result: %lf\n", ld);
+        return 1;
+    }
+
+    _Exit(0);
+
+    printf("error: _Exit() function does not work~n");
+    return 1;
+}
