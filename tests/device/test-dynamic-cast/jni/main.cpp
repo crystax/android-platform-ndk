6c20e8770fbfe4c2ecd17a082f1680d5c6455826
Alexander Zhukov <zuav@crystax.net>
2013-01-23 13:01:12 +0400

    Add test for dynamic-cast segfault.

---
+++ tests/device/test-dynamic-cast/jni/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+
+
+
+class A {
+public:
+	A();
+	virtual ~A();
+};
+
+class B : public A {
+public:
+	B();
+	~B();
+};
+
+
+A::A() {}
+A::~A() {}
+B::B() {}
+B::~B() {}
+
+
+int main()
+{
+	A *pa = dynamic_cast<A*>(new B);
+	B *pb = static_cast<B*>(pa);
+	B *pc = dynamic_cast<B*>(pa);
+
+    std::cout << "pa = " << std::hex << pa << std::endl;
+    std::cout << "pb = " << std::hex << pb << std::endl;
+    // this line will cause segfault with gcc 4.4.3 on armeabi
+    std::cout << "pc = " << std::hex << pc << std::endl;
+
+	delete pa;
+
+	return 0;
+}
