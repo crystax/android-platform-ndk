acc560c10ba50a5aaa7e3b9481aaa34da627b3d2
Alexander Zhukov <zuav@crystax.net>
2013-02-06 22:59:24 +0400

    Treat wchar as unsigned integer.
    This fixes compilation errors with clang.

--- sources/crystax/include/wchar.h
+++ sources/crystax/include/wchar.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c)1999 Citrus Project,
+ * Copyright (c)1999, 2013 Citrus Project,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -67,17 +67,17 @@
 /*
  * Copyright (c) 2011-2012 Dmitry Moskalchuk <dm@crystax.net>.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
- * 
+ *
  *    1. Redistributions of source code must retain the above copyright notice, this list of
  *       conditions and the following disclaimer.
- * 
+ *
  *    2. Redistributions in binary form must reproduce the above copyright notice, this list
  *       of conditions and the following disclaimer in the documentation and/or other materials
  *       provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY Dmitry Moskalchuk ''AS IS'' AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Dmitry Moskalchuk OR
@@ -87,7 +87,7 @@
  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are those of the
  * authors and should not be interpreted as representing official policies, either expressed
  * or implied, of Dmitry Moskalchuk.
@@ -119,12 +119,12 @@ typedef	__wint_t	wint_t;
 #define	_WINT_T_DEFINED
 #endif
 
-#ifndef	WCHAR_MIN 
-#define	WCHAR_MIN	INT_MIN
+#ifndef	WCHAR_MIN
+#define	WCHAR_MIN	0
 #endif
 
 #ifndef WCHAR_MAX
-#define	WCHAR_MAX	INT_MAX
+#define	WCHAR_MAX	UINT_MAX
 #endif
 
 #ifndef WEOF
