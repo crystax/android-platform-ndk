2c06763c1c1c02e735464642f81bfd55c1d4dec9
Alexander Zhukov <zuav@crystax.net>
2013-02-06 13:11:07 +0400

    Fix compilation errors and warnig with clang 3.1.
    Also fix some bad logic.

--- tests/device/test-stdlib/jni/main.c
+++ tests/device/test-stdlib/jni/main.c
@@ -14,7 +14,7 @@ static int test_rand_r(void);
 
 int main()
 {
-    if (!test_rand_r())
+    if (test_rand_r() != 0)
         return 1;
 
     return 0;
@@ -23,10 +23,10 @@ int main()
 
 int test_rand_r(void)
 {
-    int v0 = 0, v0_1 = 0;
-    int v1 = 0, v1_1 = 0;
-    int v2 = 0, v2_1 = 0;
-    int v3 = 0, v3_1 = 0;
+    unsigned int v0 = 0, v0_1 = 0;
+    unsigned int v1 = 0, v1_1 = 0;
+    unsigned int v2 = 0, v2_1 = 0;
+    unsigned int v3 = 0, v3_1 = 0;
 
     v1 = rand_r(&v0);
     v2 = rand_r(&v1);
@@ -41,10 +41,15 @@ int test_rand_r(void)
     v2_1 = rand_r(&v1_1);
     v3_1 = rand_r(&v2_1);
 
+    /* printf("v0_1 = %i\n", v0_1); */
+    /* printf("v1_1 = %i\n", v1_1); */
+    /* printf("v2_1 = %i\n", v2_1); */
+    /* printf("v3_1 = %i\n", v3_1); */
+
     if ((v0 != v0_1) || (v1 != v1_1) || (v2 != v2_1) || (v3 != v3_1)) {
         printf("rand_r test failed\n");
         return 1;
     }
 
-    return;
+    return 0;
 }
