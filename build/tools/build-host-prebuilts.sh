f89416d117e2a4bd99c9c746215823f1b3e8aa02
Alexander Zhukov <zuav@crystax.net>
2013-04-11 02:21:07 +0400

    Fixed --with-polly flag for llvm build.

--- build/tools/build-host-prebuilts.sh
+++ build/tools/build-host-prebuilts.sh
@@ -323,12 +323,10 @@ for SYSTEM in $SYSTEMS; do
     done
 
     # Build llvm and clang
-    # zuav todo: remove if builds ok
-    # POLLY_FLAGS=
-    # if [ "$TRY64" != "yes" -a "$SYSTEM" != "windows" ]; then
-    #     POLLY_FLAGS="--with-polly"
-    # fi
-    POLLY_FLAGS="--with-polly"
+    POLLY_FLAGS=
+    if [ "$TRY64" != "yes" -a "$SYSTEM" != "windows" ]; then
+        POLLY_FLAGS="--with-polly"
+    fi
     for LLVM_VERSION in $LLVM_VERSION_LIST; do
         echo "Building $SYSNAME clang/llvm-$LLVM_VERSION"
         run $BUILDTOOLS/build-llvm.sh "$SRC_DIR" "$NDK_DIR" "llvm-$LLVM_VERSION" $TOOLCHAIN_FLAGS $POLLY_FLAGS $CHECK_FLAG
