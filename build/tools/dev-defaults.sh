6317ca6cb91b7cbbb19e7e99bf1f5bca11e51424
Dmitry Moskalchuk <dm@crystax.net>
2014-09-24 19:19:40 +0400

    Use binutils-2.24 for all mips/mips64 toolchains

--- build/tools/dev-defaults.sh
+++ build/tools/dev-defaults.sh
@@ -254,7 +254,8 @@ get_toolchain_name_list_for_arch ()
 get_default_binutils_version_for_gcc ()
 {
     case $1 in
-        mipsel-*-4.4.3|*-4.6) echo "$DEFAULT_BINUTILS_VERSION";;
+        mips*) echo "2.24";;
+        *-4.6) echo "$DEFAULT_BINUTILS_VERSION";;
         *-4.4.3) echo "2.19";;
         x86*-4.7) echo "2.23";;  # Use 2.23 to get x32 support in ld.gold
         *-4.7) echo "2.22";;
