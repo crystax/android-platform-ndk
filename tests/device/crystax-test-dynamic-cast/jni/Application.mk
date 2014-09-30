43a947644d82a2b440f7649b772920fb73c500bf
Dmitry Moskalchuk <dm@crystax.net>
2014-09-30 19:33:07 +0400

    Fix test case tests/device/crystax-test-dynamic-cast

--- tests/device/crystax-test-dynamic-cast/jni/Application.mk
+++ tests/device/crystax-test-dynamic-cast/jni/Application.mk
@@ -1,13 +1,2 @@
 APP_ABI := all
-APP_PLATFORM := android-8
-APP_STL := gnustl_shared
-
-#_static
-#APP_MODULES := stlport
-#STLPORT_FORCE_REBUILD := true
-
-#APP_CPPFLAGS += -fexceptions
-#APP_CPPFLAGS += -frtti
-
-
-
+APP_CPPFLAGS += -frtti
--- tests/device/crystax-test-dynamic-cast/jni/main.cpp
+++ tests/device/crystax-test-dynamic-cast/jni/main.cpp
@@ -1,47 +1,46 @@
 #include <stdio.h>
-// #include <iostream>
-// #include <iomanip>
-
-
 
 class A {
 public:
-	A();
-	virtual ~A();
+    A();
+    virtual ~A();
 };
 
 class B : public A {
 public:
-	B();
-	~B();
+    B();
+    ~B();
 };
-
 
 A::A() {}
 A::~A() {}
 B::B() {}
 B::~B() {}
 
-
 int main()
 {
-	A *pa = dynamic_cast<A*>(new B);
-	B *pb = static_cast<B*>(pa);
-	B *pc = dynamic_cast<B*>(pa);
+    A *pa = dynamic_cast<A*>(new B);
+    B *pb = static_cast<B*>(pa);
+    B *pc = dynamic_cast<B*>(pa);
 
-    printf("pa = %x\n", pa);
-    printf("pb = %x\n", pb);
+    ::printf("pa = %p\n", pa);
+    ::printf("pb = %p\n", pb);
     // this line will cause segfault with gcc 4.4.3 on armeabi
-    printf("pc = %x\n", pc);
+    ::printf("pc = %p\n", pc);
 
-    /*
-    std::cout << "pa = " << std::hex << pa << std::endl;
-    std::cout << "pb = " << std::hex << pb << std::endl;
-    // this line will cause segfault with gcc 4.4.3 on armeabi
-    std::cout << "pc = " << std::hex << pc << std::endl;
-    */
+    if (pa != pb)
+    {
+        ::fprintf(stderr, "*** ERROR: pa != pb\n");
+        return 1;
+    }
 
-	delete pa;
+    if (pa != pc)
+    {
+        ::fprintf(stderr, "*** ERROR: pa != pc\n");
+        return 1;
+    }
 
-	return 0;
+    delete pa;
+
+    return 0;
 }
