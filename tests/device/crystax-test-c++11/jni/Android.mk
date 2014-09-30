1363ac315dff24b4eaacd232669a9bb3d7610c0b
Dmitry Moskalchuk <dm@crystax.net>
2014-09-30 19:27:17 +0400

    Fix settings of tests/device/crystax-test-c++11

--- tests/device/crystax-test-c++11/jni/Android.mk
+++ tests/device/crystax-test-c++11/jni/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := crystax-test-c++11
 LOCAL_SRC_FILES := main.cpp                 \
+                   test-language.cpp        \
                    test-chrono-duration.cpp \
                    test-thread.cpp          \
                    test-to-string.cpp       \
--- tests/device/crystax-test-c++11/jni/Application.mk
+++ tests/device/crystax-test-c++11/jni/Application.mk
@@ -1,3 +1,3 @@
 APP_ABI := all
-APP_PLATFORM := android-9
-APP_USE_CXX11 := true
+#APP_PLATFORM := android-9
+#APP_USE_CXX11 := true
