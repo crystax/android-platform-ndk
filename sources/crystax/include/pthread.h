28e0775ae67ecf677ca3680f4ce7e1bffc348ced
Dmitry Moskalchuk <dm@crystax.net>
2014-10-01 11:48:57 +0400

    Add pthread.h to libcrystax includes
    Define pthread_mutex_timedlock there, since some Google's headers absent it.

---
+++ sources/crystax/include/pthread.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2011-2014 CrystaX .NET.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CrystaX .NET ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL CrystaX .NET OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of CrystaX .NET.
+ */
+
+#ifndef __CRYSTAX_PTHREAD_H_B2421410F8124BED8ACB3D385041AECF
+#define __CRYSTAX_PTHREAD_H_B2421410F8124BED8ACB3D385041AECF
+
+#include <crystax/google/pthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if !defined(__LP64__) || !__LP64__
+int pthread_mutex_timedlock(pthread_mutex_t *, struct timespec*);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CRYSTAX_PTHREAD_H_B2421410F8124BED8ACB3D385041AECF */
