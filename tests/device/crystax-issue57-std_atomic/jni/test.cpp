fcd057b3b035d279aa9b9498da38d9faa8703774
Dmitry Moskalchuk <dm@crystax.net>
2014-10-09 20:32:31 +0400

    Enable on-host testing for tests/device/crystax-issue57-std_atomic

--- tests/device/crystax-issue57-std_atomic/jni/test.cpp
+++ tests/device/crystax-issue57-std_atomic/jni/test.cpp
@@ -3,14 +3,17 @@
 
 int main()
 {
+    printf("std::atomic test - begin\n");
+
     std::atomic<void*> atomicPtr;
     void *ptr = (void*) 0x12345678;
     atomicPtr = ptr;
     if ((void*)ptr != (void*)atomicPtr)
     {
-        printf("ERROR: ptr=%p, atomicPtr=%p\n", (void*)ptr, (void*)atomicPtr);
+        fprintf(stderr, "ERROR: ptr=%p, atomicPtr=%p\n", (void*)ptr, (void*)atomicPtr);
         return 1;
     }
 
+    printf("OK\n");
     return 0;
 }
