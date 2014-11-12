982321a65327f3ca075fb1a161815fe8850cacd3
Dmitry Moskalchuk <dm@crystax.net>
2014-11-13 01:19:07 +0300

    Fix test-libc++ case (shared variant)

--- tests/device/test-libc++/jni/Android.mk
+++ tests/device/test-libc++/jni/Android.mk
@@ -1,15 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := test_1_shared
+LOCAL_MODULE := test_1_static
 LOCAL_SRC_FILES := test_1.cc
 LOCAL_STATIC_LIBRARIES := libc++_static
+LOCAL_STATIC_LIBRARIES += crystax_static
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := test_1_static
+LOCAL_MODULE := test_1_shared
 LOCAL_SRC_FILES := test_1.cc
 LOCAL_SHARED_LIBRARIES := libc++_shared
+LOCAL_SHARED_LIBRARIES += crystax_shared
 include $(BUILD_EXECUTABLE)
 
+$(call import-module,crystax)
 $(call import-module,cxx-stl/llvm-libc++)
--- tests/device/test-libc++/jni/Application.mk
+++ tests/device/test-libc++/jni/Application.mk
@@ -1,3 +1,4 @@
 APP_ABI := all
 APP_STL := none
+APP_CRYSTAX := none
 LIBCXX_FORCE_REBUILD := true
