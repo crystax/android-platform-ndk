6ac7a875282a241efeead6bf83f88eac6a579ba8
Dmitry Moskalchuk <dm@crystax.net>
2014-11-13 02:24:12 +0300

    Speed up test case crystax-test-headers

--- tests/build/crystax-test-headers/build.sh
+++ tests/build/crystax-test-headers/build.sh
@@ -43,43 +43,116 @@ abis_for_platform()
     fi
 }
 
+ext_for_lang()
+{
+    local lang=$1
+    if [ -z "$lang" ]; then
+        echo "ERROR: You should pass languages to call of ext_for_lang" 1>&2
+        exit 1
+    fi
+
+    case $lang in
+        c)      echo "c"   ;;
+        c++)    echo "cpp" ;;
+        objc)   echo "m"   ;;
+        objc++) echo "mm"  ;;
+        *)
+            echo "ERROR: Unknown language: '$lang'" 1>&2
+            exit 1
+    esac
+}
+
 run()
 {
     echo "## COMMAND: $@"
     "$@"
 }
 
-for HEADER in $HEADERS; do
-    HEADER=${HEADER##$INCDIR/}
+for PLATFORM in $PLATFORMS; do
+    for ABI in $(abis_for_platform $PLATFORM); do
+        echo ""
+        echo "================================================================="
+        echo "===> Testing CrystaX headers for $PLATFORM/$ABI <==="
+        echo "================================================================="
+        echo ""
 
-    # Skip internal headers
-    [[ ${HEADER} == "_ctype.h"    ]] && continue
-    [[ ${HEADER##crystax/arm64/}   != $HEADER ]] && continue
-    [[ ${HEADER##crystax/details}  != $HEADER ]] && continue
-    [[ ${HEADER##crystax/freebsd/} != $HEADER ]] && continue
-    [[ ${HEADER##crystax/mips64/}  != $HEADER ]] && continue
-    [[ ${HEADER##crystax/sys/}     != $HEADER ]] && continue
-    [[ ${HEADER##linux/}           != $HEADER ]] && continue
-    [[ ${HEADER##machine/}         != $HEADER ]] && continue
-    [[ ${HEADER##sys/_}            != $HEADER ]] && continue
-    [[ ${HEADER##xlocale/}         != $HEADER ]] && continue
+        rm -Rf jni obj libs
+        mkdir -p jni || exit 1
 
-    for PLATFORM in $PLATFORMS; do
-        for ABI in $(abis_for_platform $PLATFORM); do
-            echo ""
-            echo "==================================================================================="
-            echo "===> Compiling CrystaX header $HEADER for $PLATFORM/$ABI <==="
-            echo "==================================================================================="
-            echo ""
-            run $NDK/ndk-build -C $MYDIR -B APP_CFLAGS=-DHEADER=\"\<$HEADER\>\" APP_PLATFORM=$PLATFORM APP_ABI=$ABI HEADER=$HEADER V=1
-            if [ $? -ne 0 ]; then
-                echo "ERROR: Can't compile CrystaX header <$HEADER> alone" 1>&2
-                exit 1
-            fi
-            echo ""
-            echo "===> OK: CrystaX header $HEADER for $PLATFORM/$ABI"
+        echo 'APP_ABI := all' >jni/Application.mk || exit 1
+
+        {
+            echo 'LOCAL_PATH := $(call my-dir)'
+            echo ''
+            echo 'include $(CLEAR_VARS)'
+            echo 'LOCAL_MODULE := crystax-test-headers'
+            echo 'LOCAL_SRC_FILES := main.c'
+        } | cat >jni/Android.mk || exit 1
+
+        {
+            echo 'int main() { return 0; }'
+        } | cat >jni/main.c || exit 1
+
+        num=0
+
+        for HEADER in $HEADERS; do
+            HEADER=${HEADER##$INCDIR/}
+
+            # Skip internal headers
+            [[ ${HEADER} == "_ctype.h"    ]] && continue
+            [[ ${HEADER##crystax/arm64/}   != $HEADER ]] && continue
+            [[ ${HEADER##crystax/details}  != $HEADER ]] && continue
+            [[ ${HEADER##crystax/freebsd/} != $HEADER ]] && continue
+            [[ ${HEADER##crystax/mips64/}  != $HEADER ]] && continue
+            [[ ${HEADER##crystax/sys/}     != $HEADER ]] && continue
+            [[ ${HEADER##linux/}           != $HEADER ]] && continue
+            [[ ${HEADER##machine/}         != $HEADER ]] && continue
+            [[ ${HEADER##sys/_}            != $HEADER ]] && continue
+            [[ ${HEADER##xlocale/}         != $HEADER ]] && continue
+
+            num=$(expr $num + 1)
+
+            for LANG in c c++ objc objc++; do
+                fname=test${num}-${LANG}.$(ext_for_lang $LANG)
+
+                {
+                    echo "#include <${HEADER}>"
+                    echo ''
+                    echo "#if !defined(__LIBCRYSTAX) || __LIBCRYSTAX != 1"
+                    echo "#error \"__LIBCRYSTAX macro is not defined\""
+                    echo "#endif"
+                } | cat >jni/$fname || exit 1
+
+                SKIP=no
+                case $LANG in
+                    c|objc)
+                        echo $HEADER | grep -q '\.hpp$'
+                        if [ $? -eq 0 ]; then
+                            SKIP=yes
+                        fi
+                        ;;
+                esac
+                if [ "$SKIP" != "yes" ]; then
+                    echo "LOCAL_SRC_FILES += $fname" >>jni/Android.mk || exit 1
+                fi
+            done
+
         done
+
+        {
+            echo 'include $(BUILD_EXECUTABLE)'
+        } | cat >>jni/Android.mk || exit 1
+
+        run $NDK/ndk-build -C $MYDIR -B APP_PLATFORM=$PLATFORM APP_ABI=$ABI V=1
+        if [ $? -ne 0 ]; then
+            echo "ERROR: Can't compile CrystaX headers for $PLATFORM/$ABI alone" 1>&2
+            exit 1
+        fi
+        echo ""
+        echo "===> OK: CrystaX headers for $PLATFORM/$ABI"
     done
 done
+
+rm -Rf jni obj libs
 
 exit 0
--- tests/build/crystax-test-headers/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := crystax-test-headers
-LOCAL_SRC_FILES := test.c test-c++.cpp test-objc++.mm
-ifeq (,$(filter %.hpp,$(HEADER)))
-LOCAL_SRC_FILES += test-c.c test-objc.m
-endif
-include $(BUILD_EXECUTABLE)
