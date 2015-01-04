c1ff1bcb7c461afa47be9951d1c4e0f4ce3bf107
Dmitry Moskalchuk <dm@crystax.net>
2015-01-04 18:56:55 +0300

    Fix build of Boost

--- build/tools/build-boost.sh
+++ build/tools/build-boost.sh
@@ -68,16 +68,20 @@ register_jobs_option
 
 extract_parameters "$@"
 
-BOOSTDIR=$(echo $PARAMETERS | sed 1q)
-if [ -z "$BOOSTDIR" ]; then
+BOOST_SRCDIR=$(echo $PARAMETERS | sed 1q)
+if [ -z "$BOOST_SRCDIR" ]; then
     echo "ERROR: Please provide the path to the Boost source tree. See --help"
     exit 1
 fi
 
-if [ ! -d "$BOOSTDIR" ]; then
-    echo "ERROR: Not a directory: '$BOOSTDIR'"
+if [ ! -d "$BOOST_SRCDIR" ]; then
+    echo "ERROR: Not a directory: '$BOOST_SRCDIR'"
     exit 1
 fi
+
+BOOST_DSTDIR=$NDK_DIR/$BOOST_SUBDIR/$BOOST_VERSION
+mkdir -p $BOOST_DSTDIR|| exit 1
+fail_panic "Could not create Boost $BOOST_VERSION destination directory: $BOOST_DSTDIR"
 
 ABIS=$(commas_to_spaces $ABIS)
 
@@ -89,6 +93,17 @@ fi
 rm -rf "$BUILD_DIR"
 mkdir -p "$BUILD_DIR"
 fail_panic "Could not create build directory: $BUILD_DIR"
+
+mktool()
+{
+    local tool
+    for tool in "$@"; do
+        cat >$tool
+        fail_panic "Could not create tool $tool"
+        chmod +x $tool
+        fail_panic "Could not chmod +x $tool"
+    done
+}
 
 # $1: ABI
 # $2: build directory
@@ -104,7 +119,17 @@ build_boost_for_abi ()
         V=1
     fi
 
-    dump "Building $TYPE $ABI Boost libraries"
+    dump "Building Boost $BOOST_VERSION $TYPE $ABI libraries"
+
+    local APILEVEL=9
+    if [ ${ABI%%64*} != ${ABI} ]; then
+        APILEVEL=21
+    fi
+
+    local PLATFORM="android-$APILEVEL"
+
+    prepare_target_build $ABI $PLATFORM $NDK_DIR
+    fail_panic "Could not setup target build"
 
     rm -Rf $BUILDDIR
     mkdir -p $BUILDDIR
@@ -156,29 +181,41 @@ build_boost_for_abi ()
             ARCH=$ABI
     esac
 
-    local ARCHFLAGS
+    local FLAGS LFLAGS
     case $ABI in
         armeabi)
-            ARCHFLAGS="-march=armv5te -mtune=xscale -msoft-float"
+            FLAGS="-march=armv5te -mtune=xscale -msoft-float"
             ;;
         armeabi-v7a)
-            ARCHFLAGS="-march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp"
+            FLAGS="-march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp"
+            LFLAGS="-Wl,--fix-cortex-a8"
             ;;
         armeabi-v7a-hard)
-            ARCHFLAGS="-march=armv7-a -mfpu=vfpv3-d16 -mhard-float"
+            FLAGS="-march=armv7-a -mfpu=vfpv3-d16 -mhard-float"
+            LFLAGS="-Wl,--fix-cortex-a8"
+            LFLAGS="$LFLAGS -Wl,--no-warn-mismatch"
             ;;
-        *)
-            ARCHFLAGS=""
+        arm64-v8a)
+            FLAGS=""
+            ;;
+        x86)
+            FLAGS="-m32"
+            ;;
+        x86_64)
+            FLAGS="-m64"
+            ;;
+        mips)
+            FLAGS="-mabi=32 -mips32"
+            ;;
+        mips64)
+            FLAGS="-mabi=64 -mips64r6"
+            ;;
     esac
-
-    local APILEVEL=9
-    if [ ${ABI%%64*} != ${ABI} ]; then
-        APILEVEL=21
-    fi
 
     local TCPATH=$NDK_DIR/toolchains/$TCNAME-$TOOLCHAIN_VERSION/prebuilt/$HOST_TAG
 
-    cd $BOOSTDIR || exit 1
+    local SAVED_CWD=$(pwd)
+    cd $BOOST_SRCDIR || exit 1
 
     local BOOSTGENFILES="project-config.jam user-config.jam b2 bjam bootstrap.log"
     BOOSTGENFILES="$BOOSTGENFILES tools/build/src/engine/bin.*"
@@ -187,112 +224,155 @@ build_boost_for_abi ()
     rm -Rf $BOOSTGENFILES || exit 1
     trap "rm -Rf $BOOSTGENFILES" EXIT INT QUIT TERM ABRT
 
-    run ./bootstrap.sh --without-icu || exit 1
+    if [ ! -x ./b2 ]; then
+        local TMPHOSTTCDIR=$BUILDDIR/host-bin
+        run mkdir $TMPHOSTTCDIR || exit 1
 
-    cat >project-config.jam <<EOF
-import option ;
-import feature ;
+        {
+            echo "#!/bin/sh"
+            echo ""
+            echo "exec $CC $HOST_CFLAGS $HOST_LDFLAGS \"\$@\""
+        } | mktool $TMPHOSTTCDIR/cc
 
-using gcc : $ARCH : g++ ;
+        PATH=$TMPHOSTTCDIR:$SAVED_PATH
+        export PATH
 
-project : default-build <toolset>gcc ;
+        run ./bootstrap.sh --with-toolset=cc --without-icu
+        local RC=$?
+        if [ -e bootstrap.log ]; then
+            mv -f bootstrap.log $BUILDDIR/
+        fi
+        if [ $RC -eq 0 ]; then
+            true
+        else
+            false
+        fi
+        fail_panic "Could not bootstrap Boost build"
 
-libraries = ;
+        # Remove unneeded intermediate files
+        rm -Rf bjam
+        rm -Rf tools/build/src/engine/bin.*
+        rm -Rf tools/build/src/engine/bootstrap
+    fi
 
-# Stop on first error
-option.set keep-going : false ;
-EOF
-    test $? -eq 0 || exit 1
+    {
+        echo "import option ;"
+        echo "import feature ;"
+        echo "using gcc : $ARCH : g++ ;"
+        echo "project : default-build <toolset>gcc ;"
+        echo "libraries = ;"
+        echo "option.set keep-going : false ;"
+    } | cat >project-config.jam
+    fail_panic "Could not create project-config.jam"
 
-    cat >user-config.jam <<EOF
-using mpi ;
-EOF
-    test $? -eq 0 || exit 1
+    {
+        echo "using mpi ;"
+    } | cat >user-config.jam
+    fail_panic "Could not create user-config.jam"
 
-    local TMPTCDIR=$BUILDDIR/bin
-    run mkdir $TMPTCDIR || exit 1
+    local TMPTARGETTCDIR=$BUILDDIR/target-bin
+    run mkdir $TMPTARGETTCDIR || exit 1
+
+    local SYSROOT=$NDK_DIR/platforms/android-$APILEVEL/arch-$ARCH
+    local LIBCRYSTAX=$NDK_DIR/$CRYSTAX_SUBDIR
+
+    FLAGS="$FLAGS --sysroot=$SYSROOT"
 
     local TOOL
 
-    # gcc
-    TOOL=$TMPTCDIR/gcc
-    cat >$TOOL <<EOF
+    for TOOL in gcc g++ c++ cpp; do
+        mktool $TMPTARGETTCDIR/$TOOL <<EOF
 #!/bin/sh
 
-exec $TCPATH/bin/$TCPREFIX-gcc $ARCHFLAGS "\$@"
+if echo "\$@" | tr ' ' '\\n' | grep -q -x -e -shared; then
+    LINKER=yes
+elif echo "\$@" | tr ' ' '\\n' | grep -q -e "^-Wl,"; then
+    LINKER=yes
+elif echo "\$@" | tr ' ' '\\n' | grep -q -e "^-l"; then
+    LINKER=yes
+else
+    LINKER=no
+fi
+
+# Remove any -m32/-m64 from input parameters
+PARAMS=\`echo "\$@" | tr ' ' '\\n' | grep -v -x -e -m32 | grep -v -x -e -m64 | tr '\\n' ' '\`
+if [ "x\$LINKER" = "xyes" ]; then
+    # Fix SONAME for shared boost libraries
+    PARAMS=\`echo \$PARAMS | tr ' ' '\\n' | sed 's!^\\(-Wl,libboost_[^\\.]*\\.so\\)\\..*\$!\\1!' | tr '\\n' ' '\`
+fi
+
+FLAGS="$FLAGS"
+if [ "x\$LINKER" = "xyes" ]; then
+    FLAGS="\$FLAGS $LFLAGS"
+    FLAGS="\$FLAGS -L$LIBCRYSTAX/libs/$ABI"
+else
+    FLAGS="\$FLAGS -I$LIBCRYSTAX/include"
+fi
+
+exec $TCPATH/bin/$TCPREFIX-$TOOL \$FLAGS \$PARAMS
 EOF
-    test $? -eq 0 || exit 1
-    chmod +x $TOOL || exit 1
+        fail_panic "Could not create target tool $TOOL"
+    done
 
-    # g++
-    TOOL=$TMPTCDIR/g++
-    cat >$TOOL <<EOF
-#!/bin/sh
+    for TOOL in as ar ranlib strip gcc-ar gcc-ranlib; do
+        {
+            echo "#!/bin/sh"
+            echo "exec $TCPATH/bin/$TCPREFIX-$TOOL \"\$@\""
+        } | mktool $TMPTARGETTCDIR/$TOOL
+        fail_panic "Could not create target tool $TOOL"
+    done
 
-PARAMS=\`echo "\$@" | tr ' ' '\\n' | sed 's!^\\(-Wl,libboost_[^\\.]*\\.so\\)\\..*\$!\\1!' | tr '\\n' ' '\`
-exec $TCPATH/bin/$TCPREFIX-g++ $ARCHFLAGS \$PARAMS
-EOF
-    test $? -eq 0 || exit 1
-    chmod +x $TOOL || exit 1
-
-    # cpp
-    TOOL=$TMPTCDIR/cpp
-    cat >$TOOL <<EOF
-#!/bin/sh
-
-exec $TCPATH/bin/$TCPREFIX-cpp $ARCHFLAGS "\$@"
-EOF
-    test $? -eq 0 || exit 1
-    chmod +x $TOOL || exit 1
-
-    # as
-    TOOL=$TMPTCDIR/as
-    cat >$TOOL <<EOF
-#!/bin/sh
-
-exec $TCPATH/bin/$TCPREFIX-as "\$@"
-EOF
-    test $? -eq 0 || exit 1
-    chmod +x $TOOL || exit 1
-
-    # ar
-    TOOL=$TMPTCDIR/ar
-    cat >$TOOL <<EOF
-#!/bin/sh
-
-exec $TCPATH/bin/$TCPREFIX-ar "\$@"
-EOF
-    test $? -eq 0 || exit 1
-    chmod +x $TOOL || exit 1
-
-    # ranlib
-    TOOL=$TMPTCDIR/ranlib
-    cat >$TOOL <<EOF
-#!/bin/sh
-
-exec $TCPATH/bin/$TCPREFIX-ranlib "\$@"
-EOF
-    test $? -eq 0 || exit 1
-    chmod +x $TOOL || exit 1
-
-    PATH=$TMPTCDIR:$SAVED_PATH
+    PATH=$TMPTARGETTCDIR:$SAVED_PATH
     export PATH
 
-    local SYSROOT=$NDK_DIR/platforms/android-$APILEVEL/arch-$ARCH
-    local GNULIBCXX=$NDK_DIR/sources/cxx-stl/gnu-libstdc++/$TOOLCHAIN_VERSION
-
-    local EXTRA_FLAGS=""
+    local BJAMABI
     case $ARCH in
         arm)
-            EXTRA_FLAGS="$EXTRA_FLAGS abi=aapcs binary-format=elf"
+            BJAMARCH=arm
+            BJAMABI=aapcs
+            ;;
+        x86|x86_64)
+            BJAMARCH=x86
+            BJAMABI=sysv
+            ;;
+        mips)
+            BJAMARCH=mips1
+            BJAMABI=o32
             ;;
     esac
 
-    if [ "$TYPE" = "shared" ]; then
-        EXTRA_FLAGS="$EXTRA_FLAGS linkflags=-lgnustl_shared"
+    local BJAMADDRMODEL
+    if [ ${ARCH%%64} != ${ARCH} ]; then
+        BJAMADDRMODEL=64
+    else
+        BJAMADDRMODEL=32
     fi
 
+    local EXTRA_OPTIONS
+    case $ARCH in
+        arm|x86|x86_64|mips)
+            EXTRA_OPTIONS="$EXTRA_OPTIONS \
+                architecture=$BJAMARCH \
+                abi=$BJAMABI \
+                binary-format=elf \
+                address-model=$BJAMADDRMODEL \
+                "
+            ;;
+    esac
+
+    local WITHOUT="--without-python"
+    case $ARCH in
+        arm64|mips64)
+            WITHOUT="$WITHOUT \
+                --without-context \
+                --without-coroutine \
+                "
+            ;;
+    esac
+
     local PREFIX=$BUILDDIR/install
+
+    local GNULIBCXX=$NDK_DIR/sources/cxx-stl/gnu-libstdc++/$TOOLCHAIN_VERSION
 
     run ./b2 -d+2 -q -j$NUM_JOBS \
         variant=release \
@@ -302,37 +382,30 @@ EOF
         threadapi=pthread \
         target-os=android \
         toolset=gcc-$ARCH \
-        architecture=$ARCH \
         include=$GNULIBCXX/include \
         include=$GNULIBCXX/libs/$ABI/include \
-        include=$NDK_DIR/$CRYSTAX_SUBDIR/include \
         library-path=$GNULIBCXX/libs/$ABI \
-        library-path=$NDK_DIR/$CRYSTAX_SUBDIR/libs/$ABI \
-        cflags="--sysroot=$SYSROOT" \
-        cxxflags="--sysroot=$SYSROOT" \
-        linkflags="--sysroot=$SYSROOT" \
-        $EXTRA_FLAGS \
-        --without-python \
+        linkflags="-lgnustl_${TYPE}" \
+        $EXTRA_OPTIONS \
         --layout=system \
         --prefix=$PREFIX \
         --build-dir=$BUILDDIR/build \
+        $WITHOUT \
         install \
         || exit 1
 
     rm -Rf $BOOSTGENFILES || exit 1
 
-    local INSTALL=$NDK_DIR/$BOOST_SUBDIR/$BOOST_VERSION
-
-    mkdir -p $INSTALL || exit 1
-
     if [ "$BOOST_HEADERS_INSTALLED" != "yes" ]; then
-        run rm -Rf $INSTALL/include || exit 1
-        run cp -pR $PREFIX/include $INSTALL/ || exit 1
+        log "Install Boost $BOOST_VERSION headers into $BOOST_DSTDIR"
+        run rm -Rf $BOOST_DSTDIR/include || exit 1
+        run cp -pR $PREFIX/include $BOOST_DSTDIR/ || exit 1
         BOOST_HEADERS_INSTALLED=yes
         export BOOST_HEADERS_INSTALLED
     fi
 
-    run mkdir -p $INSTALL/libs/$ABI || exit 1
+    log "Install Boost $BOOST_VERSION $TYPE $ABI libraries into $BOOST_DSTDIR"
+    run mkdir -p $BOOST_DSTDIR/libs/$ABI || exit 1
 
     local LIBSUFFIX
     if [ "$TYPE" = "shared" ]; then
@@ -340,24 +413,43 @@ EOF
     else
         LIBSUFFIX=a
     fi
-    run rm -f $INSTALL/libs/$ABI/lib*.$LIBSUFFIX || exit 1
+    run rm -f $BOOST_DSTDIR/libs/$ABI/lib*.$LIBSUFFIX || exit 1
 
     for f in $(find $PREFIX -name "lib*.$LIBSUFFIX" -print); do
-        run cp -pRH $f $INSTALL/libs/$ABI/$(basename $f) || exit 1
+        run cp -pRH $f $BOOST_DSTDIR/libs/$ABI/$(basename $f) || exit 1
     done
+
+    cd $SAVED_CWD || exit 1
+
+    log "Boost $BOOST_VERSION $TYPE $ABI binaries built successfully"
 }
 
 SAVED_PATH=$PATH
+
+if [ -n "$PACKAGE_DIR" ]; then
+    PACKAGE_NAME="boost-$BOOST_VERSION-headers.tar.bz2"
+    echo "Look for: $PACKAGE_NAME"
+    try_cached_package "$PACKAGE_DIR" "$PACKAGE_NAME" no_exit
+    if [ $? -eq 0 ]; then
+        BOOST_HEADERS_NEED_PACKAGE=no
+    else
+        BOOST_HEADERS_NEED_PACKAGE=yes
+    fi
+fi
 
 BUILT_ABIS=""
 for ABI in $ABIS; do
     DO_BUILD_PACKAGE="yes"
     if [ -n "$PACKAGE_DIR" ]; then
-        PACKAGE_NAME="boost-libs-$ABI.tar.bz2"
+        PACKAGE_NAME="boost-$BOOST_VERSION-libs-$ABI.tar.bz2"
         echo "Look for: $PACKAGE_NAME"
         try_cached_package "$PACKAGE_DIR" "$PACKAGE_NAME" no_exit
-        if [ $? = 0 ]; then
-            DO_BUILD_PACKAGE="no"
+        if [ $? -eq 0 ]; then
+            if [ "$BOOST_HEADERS_NEED_PACKAGE" = "yes" -a -z "$BUILT_ABIS" ]; then
+                BUILT_ABIS="$BUILT_ABIS $ABI"
+            else
+                DO_BUILD_PACKAGE="no"
+            fi
         else
             BUILT_ABIS="$BUILT_ABIS $ABI"
         fi
@@ -368,25 +460,63 @@ for ABI in $ABIS; do
     fi
 done
 
+# Restore PATH
 PATH=$SAVED_PATH
 export PATH
 
+# Copy license
+log "Copying Boost $BOOST_VERSION license"
+run cp -f $BOOST_SRCDIR/LICENSE_1_0.txt $BOOST_DSTDIR/ || exit 1
+
+# Generate Android.mk
+log "Generating $BOOST_DSTDIR/Android.mk"
+{
+    echo "# WARNING!!! THIS IS AUTO-GENERATED FILE!!! DO NOT EDIT IT MANUALLY!!!"
+    echo ""
+    cat $NDK_DIR/$CRYSTAX_SUBDIR/LICENSE | sed 's,^,# ,' | sed 's,^#\s*$,#,'
+    echo ""
+    echo 'LOCAL_PATH := $(call my-dir)'
+
+    for suffix in a so; do
+        if [ "$suffix" = "a" ]; then
+            type=static
+        else
+            type=shared
+        fi
+        find $BOOST_DSTDIR/libs -name "libboost_*.$suffix" -exec basename '{}' \; | \
+            sed "s,^lib\\([^\\.]*\\)\\.${suffix}$,\\1," | sort | uniq | \
+        {
+            while read lib; do
+                echo ''
+                echo 'include $(CLEAR_VARS)'
+                echo "LOCAL_MODULE := ${lib}_${type}"
+                echo "LOCAL_SRCFILES := libs/\$(TARGET_ARCH_ABI)/lib${lib}.${suffix}"
+                echo 'LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include'
+                echo "include \$(PREBUILT_$(echo $type | tr '[a-z]' '[A-Z]')_LIBRARY)"
+            done
+        }
+    done
+} | cat >$BOOST_DSTDIR/Android.mk
+
 # If needed, package files into tarballs
 if [ -n "$PACKAGE_DIR" ] ; then
-    for ABI in $BUILT_ABIS; do
-        FILES=""
-        for MLIB in $($NDK_DIR/$CRYSTAX_SUBDIR/bin/config --multilibs --abi=$ABI); do
-            LIBPATH=$($NDK_DIR/$CRYSTAX_SUBDIR/bin/config --libpath --abi=$ABI --multilib=$MLIB)
-            for LIB in libboost_*.so libboost_*.a; do
-                FILES="$FILES $CRYSTAX_SUBDIR/$LIBPATH/$LIB"
-            done
-        done
-        PACKAGE_NAME="boost-libs-$ABI.tar.bz2"
+    if [ "$BOOST_HEADERS_NEED_PACKAGE" = "yes" ]; then
+        FILES="$BOOST_SUBDIR/$BOOST_VERSION/include"
+        PACKAGE_NAME="boost-$BOOST_VERSION-headers.tar.bz2"
         PACKAGE="$PACKAGE_DIR/$PACKAGE_NAME"
-        log "Packaging: $PACKAGE"
-        pack_archive "$PACKAGE" "$NDK_DIR" "$FILES"
-        fail_panic "Could not package $ABI Boost binaries!"
         dump "Packaging: $PACKAGE"
+        pack_archive "$PACKAGE" "$NDK_DIR" "$FILES"
+        fail_panic "Could not package Boost $BOOST_VERSION headers!"
+        cache_package "$PACKAGE_DIR" "$PACKAGE_NAME"
+    fi
+
+    for ABI in $BUILT_ABIS; do
+        FILES="$BOOST_SUBDIR/$BOOST_VERSION/libs/$ABI"
+        PACKAGE_NAME="boost-$BOOST_VERSION-libs-$ABI.tar.bz2"
+        PACKAGE="$PACKAGE_DIR/$PACKAGE_NAME"
+        dump "Packaging: $PACKAGE"
+        pack_archive "$PACKAGE" "$NDK_DIR" "$FILES"
+        fail_panic "Could not package $ABI Boost $BOOST_VERSION binaries!"
         cache_package "$PACKAGE_DIR" "$PACKAGE_NAME"
     done
 fi
