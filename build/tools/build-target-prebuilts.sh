d51ae1cb7e9581148581131a2e87e1b0f4696e09
Dmitry Moskalchuk <dm@crystax.net>
2015-01-05 03:14:51 +0300

    Add build of Boost to the build-target-prebuilts script

--- build/tools/build-target-prebuilts.sh
+++ build/tools/build-target-prebuilts.sh
@@ -209,6 +209,10 @@ if [ ! -z "$GCC_VERSION_LIST" ]; then
   fail_panic "Could not build gnustl with debug info!"
 fi
 
+dump "Building $ABIS boost binaries..."
+run $BUILDTOOLS/build-boost.sh $FLAGS --abis="$ABIS" $(cd $SRC_DIR/../vendor/boost && pwd)
+fail_panic "Could not build Boost!"
+
 dump "Building $ABIS libportable binaries..."
 run $BUILDTOOLS/build-libportable.sh --abis="$ABIS" $FLAGS $BUILD_TOOLCHAIN
 fail_panic "Could not build libportable!"
