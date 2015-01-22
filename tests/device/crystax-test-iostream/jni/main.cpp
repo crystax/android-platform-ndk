b44ba2daeb702469ea38f0bbdfb9cf5fa85bf0cf
Dmitry Moskalchuk <dm@crystax.net>
2015-01-22 21:10:39 +0300

    Adjust device test crystax-test-iostream

--- tests/device/crystax-test-iostream/jni/main.cpp
+++ tests/device/crystax-test-iostream/jni/main.cpp
@@ -5,10 +5,26 @@
 
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 
 int main()
 {
     std::cout << "iostream - begin" << std::endl;
     std::cout << "OK" << std::endl;
+
+    std::ofstream fileOut("out");
+    fileOut << "34 34 34 34 34 34 34 34 34 34 34 34 34 34 34 34 34 34";
+    fileOut.close();
+
+    std::ifstream fileIn("out");
+    fileIn.seekg(12,std::ios::beg);
+    uint16_t data;
+    fileIn >> data;
+
+    if (data != 34) {
+        std::cerr << "ERROR: read '" << data << "' instead of 34" << std::endl;
+        return 1;
+    }
+
     return 0;
 }
