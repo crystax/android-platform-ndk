b7105887dc902cada9a0f6bfbeff04146f4795f2
Dmitry Moskalchuk <dm@crystax.net>
2015-05-12 00:54:51 +0300

    Add test case crystax-issue906-long-double
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

---
+++ tests/build/crystax-issue906-long-double/build.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+. $NDK/build/tools/dev-defaults.sh
+
+HOST_OS=$(uname -s | tr '[A-Z]' '[a-z]')
+case $HOST_OS in
+    cygwin*|mingw*)
+        HOST_OS=windows
+esac
+
+HOST_ARCH=""
+HOST_ARCH2=""
+
+case $HOST_OS in
+    darwin|linux|windows)
+        HOST_ARCH=$(uname -m)
+        HOST_ARCH2=
+        case $HOST_ARCH in
+            i?86)
+                HOST_ARCH=x86
+                ;;
+            x86_64)
+                if [ "$HOST_OS" = "linux" ]; then
+                    if file -b /bin/ls | grep -q 32-bit; then
+                        HOST_ARCH=x86
+                    else
+                        HOST_ARCH2=x86
+                    fi
+                else
+                    HOST_ARCH2=x86
+                fi
+                ;;
+            *)
+                echo "ERROR: Unsupported host CPU architecture: '$HOST_ARCH'" 1>&2
+                exit 1
+        esac
+        HOST_TAG=${HOST_OS}-${HOST_ARCH}
+        test -n "$HOST_ARCH2" && HOST_TAG2=${HOST_OS}-${HOST_ARCH2}
+        ;;
+    *)
+        echo "ERROR: Unsupported host OS: '$HOST_OS'" 1>&2
+        exit 1
+        ;;
+esac
+
+if [ "$HOST_OS" = "windows" -a "$HOST_ARCH" = "x86" ]; then
+    HOST_TAG=$HOST_OS
+else
+    HOST_TAG=${HOST_OS}-${HOST_ARCH}
+fi
+
+HOST_TAG2=""
+if [ -n "$HOST_ARCH2" ]; then
+    if [ "$HOST_OS" = "windows" -a "$HOST_ARCH2" = "x86" ]; then
+        HOST_TAG2=$HOST_OS
+    else
+        HOST_TAG2=${HOST_OS}-${HOST_ARCH2}
+    fi
+fi
+
+$NDK/ndk-build -B || exit 1
+
+for ABI in $(ls -1 libs | sort); do
+    echo "Check $ABI ..."
+
+    case $ABI in
+        armeabi*)
+            ARCH=arm
+            ;;
+        arm64-v8a)
+            ARCH=arm64
+            ;;
+        *)
+            ARCH=$ABI
+    esac
+
+    TOOLCHAIN_NAME=$(get_default_toolchain_name_for_arch $ARCH)
+    TOOLCHAIN_PREFIX=$(get_default_toolchain_prefix_for_arch $ARCH)
+
+    for tag in $HOST_TAG $HOST_TAG2; do
+        NM=$NDK/toolchains/$TOOLCHAIN_NAME/prebuilt/$tag/bin/${TOOLCHAIN_PREFIX}-nm
+        echo "Probing for ${NM}..."
+        if [ -x $NM ]; then
+            echo "Found: $NM"
+            break
+        fi
+    done
+
+    if [ ! -x "$NM" ]; then
+        echo "ERROR: Can't find $ARCH nm" 1>&2
+        exit 1
+    fi
+
+    $NM -D libs/$ABI/libtest.so | grep -q "^ *U _ZTIe\>"
+    if [ $? -ne 0 ]; then
+        echo "ERROR: $ABI library don't contain 'typeinfo for long double' reference" 1>&2
+        exit 1
+    fi
+
+    echo "OK"
+done
+
+echo "Done!"
---
+++ tests/build/crystax-issue906-long-double/jni/Android.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := test
+LOCAL_SRC_FILES := test.cpp
+include $(BUILD_SHARED_LIBRARY)
