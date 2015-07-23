19e4513b2250a77c62ab6171c7e352fb01cdb24e
Dmitry Moskalchuk <dm@crystax.net>
2015-07-23 19:42:12 +0300

    [libcrystax] Fix bug #1004
    Add own implementation of termios functions, including tcdrain().
    
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- sources/crystax/include/termios.h
+++ sources/crystax/include/termios.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2014 CrystaX .NET.
+ * Copyright (c) 2011-2015 CrystaX .NET.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
@@ -27,40 +27,32 @@
  * or implied, of CrystaX .NET.
  */
 
-#include <termios.h>
+#ifndef __CRYSTAX_INCLUDE_TERMIOS_H_4BB725BB15A04872B58EBC8A4F9321F0
+#define __CRYSTAX_INCLUDE_TERMIOS_H_4BB725BB15A04872B58EBC8A4F9321F0
 
-#include "gen/termios.inc"
+#include <crystax/id.h>
 
-#include "helper.h"
+#include <sys/cdefs.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <linux/termios.h>
 
-#define CHECK(type) type JOIN(termios_check_type_, __LINE__)
+__BEGIN_DECLS
 
-CHECK(cc_t);
-CHECK(speed_t);
-CHECK(tcflag_t);
-CHECK(struct termios);
-CHECK(pid_t);
+speed_t cfgetispeed(const struct termios*);
+speed_t cfgetospeed(const struct termios*);
+void cfmakeraw(struct termios*);
+int cfsetispeed(struct termios*, speed_t);
+int cfsetospeed(struct termios*, speed_t);
+int cfsetspeed(struct termios*, speed_t);
+int tcdrain(int);
+int tcflow(int, int);
+int tcflush(int, int);
+int tcgetattr(int, struct termios*);
+pid_t tcgetsid(int);
+int tcsendbreak(int, int);
+int tcsetattr(int, int, const struct termios*);
 
-void termios_check_fields(struct termios *s)
-{
-    s->c_iflag = (tcflag_t)0;
-    s->c_oflag = (tcflag_t)0;
-    s->c_cflag = (tcflag_t)0;
-    s->c_lflag = (tcflag_t)0;
-    s->c_cc[0] = (cc_t)0;
-}
+__END_DECLS
 
-void termios_check_functions(struct termios *t, const struct termios *ct)
-{
-    (void)cfgetispeed(ct);
-    (void)cfgetospeed(ct);
-    (void)cfsetispeed(t, (speed_t)0);
-    (void)cfsetospeed(t, (speed_t)0);
-    (void)tcdrain(0);
-    (void)tcflow(0, 0);
-    (void)tcflush(0, 0);
-    (void)tcgetattr(0, t);
-    (void)tcgetsid(0);
-    (void)tcsendbreak(0, 0);
-    (void)tcsetattr(0, 0, ct);
-}
+#endif /* __CRYSTAX_INCLUDE_TERMIOS_H_4BB725BB15A04872B58EBC8A4F9321F0 */
