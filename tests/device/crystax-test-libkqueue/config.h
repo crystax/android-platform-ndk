d179468469c4d7c574a650b8b1ab53ccba3d1447
Dmitry Moskalchuk <dm@crystax.net>
2015-08-14 01:06:44 +0300

    [libcrystax] Implement kqueue API (task #1024)
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

---
+++ tests/device/crystax-test-libkqueue/config.h
@@ -0,0 +1,50 @@
+#include <android/api-level.h>
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/epoll.h> header file. */
+#define HAVE_SYS_EPOLL_H 1
+
+/* Define to 1 if you have the <sys/eventfd.h> header file. */
+#define HAVE_SYS_EVENTFD_H 1
+
+/* Define to 1 if you have the <sys/inotify.h> header file. */
+#define HAVE_SYS_INOTIFY_H 1
+
+/* Define to 1 if you have the <sys/signalfd.h> header file. */
+#if __ANDROID_API__ >= 21
+#define HAVE_SYS_SIGNALFD_H 1
+#endif
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/timerfd.h> header file. */
+#if __ANDROID_API__ >= 21
+#define HAVE_SYS_TIMERFD_H 1
+#endif
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
---
+++ tests/device/crystax-test-libkqueue/jni/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq (,$(strip $(NDK)))
+NDK := $(abspath $(LOCAL_PATH)/../../../..)
+endif
+
+TDIR := $(NDK)/sources/crystax/vendor/libkqueue/test
+
+include $(CLEAR_VARS)
+LOCAL_MODULE     := crystax-test-libkqueue
+LOCAL_SRC_FILES  := $(addprefix $(TDIR)/,\
+	main.c   \
+	kevent.c \
+	test.c   \
+	read.c   \
+	signal.c \
+	timer.c  \
+	vnode.c  \
+	user.c   \
+)
+LOCAL_CFLAGS     := -Wall -Wextra -Werror
+include $(BUILD_EXECUTABLE)
