f542705a4fb79df8d2ba66db8d3c1b5810e79a82
Dmitry Moskalchuk <dm@crystax.net>
2015-08-13 22:10:59 +0300

    [libcrystax] Implement pthread_workqueue API (task #1023)
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

---
+++ tests/device/crystax-test-pthread_workqueue/jni/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq (,$(strip $(NDK)))
+NDK := $(abspath $(LOCAL_PATH)/../../../..)
+endif
+
+TDIR := $(NDK)/sources/crystax/vendor/libpwq/testing
+
+CFLAGS := -Wall -Wextra -Werror
+CFLAGS += -DNO_CONFIG_H
+
+include $(CLEAR_VARS)
+LOCAL_MODULE     := crystax-test-pthread_workqueue-1
+LOCAL_SRC_FILES  := $(TDIR)/api/test.c
+LOCAL_CFLAGS     := $(CFLAGS)
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE     := crystax-test-pthread_workqueue-2
+LOCAL_SRC_FILES  := $(TDIR)/idle/main.c
+LOCAL_CFLAGS     := $(CFLAGS)
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE     := crystax-test-pthread_workqueue-3
+LOCAL_SRC_FILES  := $(TDIR)/latency/latency.c
+LOCAL_CFLAGS     := $(CFLAGS)
+include $(BUILD_EXECUTABLE)
