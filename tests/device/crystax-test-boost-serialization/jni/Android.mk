1caad056cb53d78ba1ab11ad1b0df563434e0e10
Dmitry Moskalchuk <dm@crystax.net>
2015-09-26 22:41:04 +0300

    [tests] Fix case crystax-test-boost-serialization
    Test both static and shared libboost_serialization
    
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- tests/device/crystax-test-boost-serialization/jni/Android.mk
+++ tests/device/crystax-test-boost-serialization/jni/Android.mk
@@ -1,9 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE           := test-boost
+LOCAL_MODULE           := test-boost-static
 LOCAL_SRC_FILES        := test.cpp gps.cpp
 LOCAL_STATIC_LIBRARIES := boost_serialization_static
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE           := test-boost-shared
+LOCAL_SRC_FILES        := test.cpp gps.cpp
+LOCAL_STATIC_LIBRARIES := boost_serialization_shared
 include $(BUILD_EXECUTABLE)
 
 $(call import-module,boost/1.57.0)
