7cc5af9b606d1ef4fc41fa05f4077469ce937d15
Dmitry Moskalchuk <dm@crystax.net>
2015-09-29 14:53:36 +0300

    [tests][clang] Fix build of ssax-instructions
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- tests/build/ssax-instructions/jni/Android.mk
+++ tests/build/ssax-instructions/jni/Android.mk
@@ -5,5 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ssax_instruction
 LOCAL_ARM_NEON := true
 LOCAL_SRC_FILES := test.S
+ifneq (,$(filter clang%,$(NDK_TOOLCHAIN_VERSION)))
+LOCAL_CFLAGS := -fno-integrated-as
+endif
 include $(BUILD_SHARED_LIBRARY)
 endif
