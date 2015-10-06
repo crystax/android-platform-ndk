6ff73baf5d6f2dd4d675327206fc8b70cda76d8f
Dmitry Moskalchuk <dm@crystax.net>
2015-10-06 08:55:20 +0300

    [boost] Remove 1.57.0 and 1.58.0
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- tests/device/crystax-test-boost-serialization/jni/Android.mk
+++ tests/device/crystax-test-boost-serialization/jni/Android.mk
@@ -12,4 +12,4 @@ LOCAL_SRC_FILES        := test.cpp gps.cpp
 LOCAL_STATIC_LIBRARIES := boost_serialization_shared
 include $(BUILD_EXECUTABLE)
 
-$(call import-module,boost/1.57.0)
+$(call import-module,boost/1.59.0)
