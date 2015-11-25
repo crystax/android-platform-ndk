d884e5424da5723d8af1222074439a26a3757fc5
Dmitry Moskalchuk <dm@crystax.net>
2015-11-25 03:32:09 +0300

    [libcrystax] Use bzlib sources from vendor/freebsd
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- sources/crystax/src/bzip2/compress.c
+++
@@ -1,8 +0,0 @@
-#include <crystax/internal.h>
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
-
-#include "../../vendor/bzip2/compress.c"
-
-#pragma GCC diagnostic pop
