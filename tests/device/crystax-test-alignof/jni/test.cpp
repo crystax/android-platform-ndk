8ec6b835d5215ec51f9f7b2ddec63b45ed922d9d
Dmitry Moskalchuk <dm@crystax.net>
2015-12-05 15:30:55 +0300

    [tests] Update crystax-test-alignof for gcc-5.3
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- tests/device/crystax-test-alignof/jni/test.cpp
+++ tests/device/crystax-test-alignof/jni/test.cpp
@@ -27,13 +27,13 @@ int main()
     // These tests failed for 32-bit x86 target being built by both gcc and clang
     // This is not Android-specific; both gcc and clang behave the same on 32-bit
     // linux and darwin targets. However, this is obviously a bug, so this test
-    // here to track it. Right now, we know gcc-4.9, gcc-5.1 and clang-3.6 are failing.
+    // here to track it. Right now, we know gcc-4.9, gcc-5.3 and clang-3.6 are failing.
     // In the future, this bug should be fixed, so next gcc/clang versions should
     // pass.
 
 #if !(defined(__i386__) && ( \
     (defined(__clang__) && (__clang_major__ < 3 || (__clang_major__ == 3 && __clang_minor__ <= 6))) || \
-    (defined(__GNUC__) && (__GNUC__ < 5 || (__GNUC__ == 5 && __GNUC_MINOR__ <= 2))) \
+    (defined(__GNUC__) && (__GNUC__ < 5 || (__GNUC__ == 5 && __GNUC_MINOR__ <= 3))) \
     ))
     TEST(long long);
     TEST(double);
