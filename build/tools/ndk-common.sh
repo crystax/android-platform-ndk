0dfca2b731a0770952dd48af72b18faf3de14a10
Alexander Zhukov <zuav@crystax.net>
2016-03-31 14:07:32 +0300

    Package only linux 64 bit release

--- build/tools/ndk-common.sh
+++ build/tools/ndk-common.sh
@@ -1,4 +1,4 @@
-# Copyright (C) 2009, 2014, 2015 The Android Open Source Project
+# Copyright (C) 2009, 2014, 2015, 2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -167,6 +167,9 @@ run ()
         echo "## SKIP COMMAND: $@"
     elif [ "$VERBOSE" = "yes" ] ; then
         echo "## COMMAND: $@"
+        if [ -n "$TMPLOG" ] ; then
+            echo "## COMMAND: $@" >> $TMPLOG
+        fi
         "$@" 2>&1
     else
         if [ -n "$TMPLOG" ] ; then
