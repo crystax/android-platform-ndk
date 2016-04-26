6d7659f5e54dbd018210fe367206d5f6045418bf
Alexander Zhukov <zuav@crystax.net>
2016-04-26 19:45:44 +0300

    Add wait3 implementation to crystax lib

--- sources/crystax/src/wait3.c
+++ sources/crystax/src/wait3.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2016 CrystaX.
+ * Copyright (c) 2016 CrystaX.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
@@ -27,25 +27,9 @@
  * or implied, of CrystaX.
  */
 
-#ifndef __CRYSTAX_SYS_WAIT_H_F9DF1DC299814368BADB2B895DD7ECB4
-#define __CRYSTAX_SYS_WAIT_H_F9DF1DC299814368BADB2B895DD7ECB4
+#include <sys/wait.h>
 
-#include <crystax/id.h>
-#include <crystax/google/sys/wait.h>
-#include <crystax/sys/wait.h>
-#include <android/api-level.h>
-#include <sys/cdefs.h>
-#include <sys/types.h>
-#include <linux/resource.h>
-
-#if __ANDROID_API__ >= 21
-
-__BEGIN_DECLS
-
-extern pid_t  wait3(int *status, int options, struct rusage *rusage);
-
-__END_DECLS
-
-#endif /* __ANDROID_API__ */
-
-#endif /* __CRYSTAX_SYS_WAIT_H_F9DF1DC299814368BADB2B895DD7ECB4 */
+pid_t  wait3(int *status, int options, struct rusage *rusage)
+{
+    return wait4(-1, status, options, rusage);
+}
