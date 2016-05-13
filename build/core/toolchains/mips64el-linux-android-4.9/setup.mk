6aa15aa9ed9cedd76df2058b2113ab1dac9f45d0
Alexander Zhukov <zuav@crystax.net>
2016-05-13 21:06:46 +0300

    Fix toolchains versions

--- build/core/toolchains/mips64el-linux-android-4.9/setup.mk
+++ build/core/toolchains/mips64el-linux-android-4.9/setup.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 The Android Open Source Project
+# Copyright (C) 2014, 2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@
 # revisions of the NDK.
 #
 
-GCC_VERSION := 6
+GCC_VERSION := 4.9
 
 TARGET_CFLAGS := \
     -fpic \
