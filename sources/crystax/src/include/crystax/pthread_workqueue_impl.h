20a3711e9d35fa8adf8cf86a5452a01378c552a0
Dmitry Moskalchuk <dm@crystax.net>
2016-05-18 15:45:09 +0300

    [libcrystax] Call libpwq constructor from __crystax_init
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

---
+++ sources/crystax/src/include/crystax/pthread_workqueue_impl.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2011-2016 CrystaX.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CrystaX ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL CrystaX OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of CrystaX.
+ */
+
+#ifndef __CRYSTAX_INCLUDE_PTHREAD_WORKQUEUE_IMPL_H_4882A12F42C64B02939E5BD9A1E45865
+#define __CRYSTAX_INCLUDE_PTHREAD_WORKQUEUE_IMPL_H_4882A12F42C64B02939E5BD9A1E45865
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+int __crystax_pthread_workqueue_init();
+
+__END_DECLS
+
+#endif /* __CRYSTAX_INCLUDE_PTHREAD_WORKQUEUE_IMPL_H_4882A12F42C64B02939E5BD9A1E45865 */
