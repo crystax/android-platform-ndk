a06607c16d855f92e338b57cb68bc3b1bd3720bf
Alexander Zhukov <zuav@crystax.net>
2017-04-04 18:12:55 +0300

    [tests] Add libjpeg library to libtiff test

--- tests/device/crystax-test-libtiff/jni/Android.mk
+++ tests/device/crystax-test-libtiff/jni/Android.mk
@@ -3,13 +3,14 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE           := test-libtiff-static
 LOCAL_SRC_FILES        := test.c
-LOCAL_STATIC_LIBRARIES := libtiff_static
+LOCAL_STATIC_LIBRARIES := libtiff_static libjpeg_static
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE           := test-libtiff-shared
 LOCAL_SRC_FILES        := test.c
-LOCAL_STATIC_LIBRARIES := libtiff_shared
+LOCAL_SHARED_LIBRARIES := libtiff_shared libjpeg_shared
 include $(BUILD_EXECUTABLE)
 
 $(call import-module,libtiff/4.0.6)
+$(call import-module,libjpeg/9b)
